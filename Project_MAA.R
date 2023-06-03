dffb4ac6140dd35805cd26c5d85b82129c807e17
Afatib <afatibadova@gmail.com>
2023-06-03 12:11:29 +0200

    I added setwd for myself.
    I get error with getwd

--- Project_MAA.R
+++ Project_MAA.R
@@ -1,9 +1,12 @@
 #House pricing Model
 ##Load Dataset
 getwd()
+#setwd('C:\\Users\\Afat\\Documents\\GitHub\\RRGroupProjectMAA') #Afet add setwd for herself
 train <- read.csv("train.csv", h=T)
 test <- read.csv("test.csv", h=T)
 row.names(train) <- train$Id
 train$Id <- NULL
 row.names(test) <- test$Id
 test$Id <- NULL
+
+### Data Exploration and Preprocessing
