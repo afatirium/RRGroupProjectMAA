15aca159d72147411cbf2af555b570952625d4ca
MinayAllahverdili <128725944+MinayAllahverdili@users.noreply.github.com>
2023-06-03 12:08:16 +0200

    Loading Dataset
    Creating directory, dropping id columns

--- Project_MAA.R
+++ Project_MAA.R
@@ -1,2 +1,9 @@
 #House pricing Model
 ##Load Dataset
+getwd()
+train <- read.csv("train.csv", h=T)
+test <- read.csv("test.csv", h=T)
+row.names(train) <- train$Id
+train$Id <- NULL
+row.names(test) <- test$Id
+test$Id <- NULL
