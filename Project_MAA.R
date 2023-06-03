1cb1b6b56b2e49671319a1eddbb5d8b1531ab780
aziz-aliev <128726363+aziz-aliev@users.noreply.github.com>
2023-06-03 12:33:55 +0200

    Plotting target variable
    Created various plots for sales price distributions

--- Project_MAA.R
+++ Project_MAA.R
@@ -10,5 +10,30 @@ row.names(test) <- test$Id
 test$Id <- NULL
 
 ### Data Exploration and Preprocessing
-summary(train$SalePrice)
+## 1. Investigate TARGET value distribution
+install.packages("vioplot")
+library(vioplot)
+par(mfrow = c(2, 2))
+par(mar = c(4, 4, 2, 1)) # Adjusting the margins for better visualization
 
+# Subplot 1
+hist(train$SalePrice, breaks = 50, main = "Sales Price Distribution", xlab = "Sale Price",
+     ylab = "Counts", col = "lightblue", border = "white")
+abline(v = mean(train$SalePrice), lty = 2, col = "red", lwd = 2)
+abline(v = median(train$SalePrice), lty = 3, col = "green", lwd = 2)
+legend("topright", legend = c("Mean", "Median"), lty = c(2, 3), col = c("red", "green"))
+
+# Subplot 2
+plot(train$SalePrice, rnorm(nrow(train), mean = 7, sd = 0.2), main = "Sales Price Distribution",
+     xlab = "Sale Price", ylab = "Random Values", pch = 16, col = "blue", cex = 0.6)
+
+# Subplot 3
+boxplot(train$SalePrice, main = "Sales Price Distribution", ylab = "Sale Price",
+        col = "lightgreen")
+
+# Subplot 4
+# Subplot 4
+install.packages("beanplot")
+library(beanplot)
+beanplot(train$SalePrice, main = "Sales Price Distribution", ylab = "Sale Price",
+         col = "lightpink", what = c(1, 1, 0, 0), bw = 0.2)
